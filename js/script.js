f2a89708f6618fc18eb113b0ae3a72e9138157ab
SabKaz78614 <shah_Zarah99@yahoo.com>
2025-03-23 16:01:28 +0000

    correct options area now shown to corresponding questions

--- js/script.js
+++ js/script.js
@@ -36,9 +36,17 @@ continueBtn.onclick = () => {
     showQuestions(questionCount);    
   }
 
+  const optionList = document.querySelector('.option-list');
 
 //getting questions and options from array
 function showQuestions(index){
     const questionText = document.querySelector('.question-text');
     questionText.textContent= `${questions[index].numb}. ${questions[index].question}`;
+
+    let optionTag = `<div class="option"><span>${questions[index].options[0]}</div>
+                     <div class="option"><span>${questions[index].options[1]}</div>
+                     <div class="option"><span>${questions[index].options[2]}</div>
+                     <div class="option"><span>${questions[index].options[3]}</div>`;
+
+    optionList.innerHTML = optionTag;
 }
