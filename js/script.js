6c7279fd8350101f4af4546a98bab99a3419b0f4
SabKaz78614 <shah_Zarah99@yahoo.com>
2025-03-23 15:07:44 +0000

    styled the quiz box and content inside quiz box to match color scheme in image

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@ const startBtn = document.querySelector('.start-btn');
 const popupInfo = document.querySelector('.popup-info');
 const exitBtn = document.querySelector('.exit-btn');
 const main = document.querySelector('.main');
+const continueBtn = document.querySelector('.continue-btn');
+const quizSection = document.querySelector('.quiz-section');
+
 
 
 startBtn.onclick = () => {
@@ -13,3 +16,9 @@ exitBtn.onclick = () => {
     popupInfo.classList.remove('active');
     main.classList.remove('active');
 }
+
+continueBtn.onclick = () => {
+    quizSection.classList.add('active');
+    popupInfo.classList.remove('active');
+    main.classList.remove('active');
+  };
